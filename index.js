61728a675166b0138b1cbcdf2727db7341538515
Oggi Danailov <ogidan@abv.bg>
2016-07-14 22:48:45 -0400

    navbar almost done

--- index.js
+++ index.js
@@ -2,36 +2,45 @@ $(document).ready(function(){
 	$("#name").hover(function(){
     $("#menu").animate({ opacity: '1.0'});
     $('body').addClass('container2');
-    $("#nav-bar").css('display', 'inline');
+    $(".clearfix").css('display', 'inline');
     $('#container').animate({opacity: 0.2}, 5000);
-    }) 
+  }) 
 
 
 	 
-function showText (target, message, index, interval) {    
-  if (index < message.length) { 
-    $(target).append(message[index++]);
-    
-    
-    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  function showText (target, message, index, interval) {    
+    if (index < message.length) { 
+      $(target).append(message[index++]); 
+      setTimeout(function () { showText(target, message, index, interval); }, interval);
+    }
   }
-}
+
+  $(function () {
+    showText("#name", "Snejana Simeonova", 0, 200); 
+  });
 
 
-$(function () {
 
-  showText("#name", "Snejana Simeonova", 0, 200); 
-    
-
+$(function() {
+  var pull = $('#pull');
+      menu = $('nav ul');
+      menuHeight  = menu.height();
+ 
+  $(pull).on('click', function(e) {
+      e.preventDefault();
+      menu.slideToggle();
+    });
 });
 
-})
+$(window).resize(function(){
+    var w = $(window).width();
+    if(w > 620 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+    }
+});
 
-function myFunction() {
-    var something = document.getElementsByClassName("topnav")[0];
-    something.classList.toggle("responsive");
-    console.log(something)
-} 
+
+}) 
 
 
 
